a3cef9f567988241feaaf1c84e4e26d90ec3ff68
Phi Le <z.phi.lecong@gree.net>
2020-02-03 08:57:38 +0700

    Just commit

--- Assets/Scripts/MapView.cs
+++ Assets/Scripts/MapView.cs
@@ -1,18 +1,39 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MapView : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+    public Button btnBack;
+    public Button btnNext;
+    [SerializeField] GameObject currentMap;
+
+    //public event Action
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnEnable()
+    {
+        btnBack.onClick.AddListener(BtnBackClick);
+        btnNext.onClick.AddListener(BtnNextClick);
+    }
+
+    void BtnBackClick()
+    {
+        currentMap = GameObject.Find("Scroll");
+        print(currentMap.transform.GetChild(0));
+        print("back");
+    }
+
+    void BtnNextClick()
+    {
+        print("next");
     }
 }
